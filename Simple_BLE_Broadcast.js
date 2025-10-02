3bf6b5f5109611b08ecebc19d7db06514451f805
Saeid <saeidsobhani1371@gmail.com>
2025-10-02 19:29:05 +0200

    implement 100 Hz data streaming in JavaScript  and change the accelerometer streaming file to use the I2C method to increase the data rate

--- Simple_BLE_Broadcast.js
+++ Simple_BLE_Broadcast.js
@@ -1,5 +1,4 @@
 // Simple BLE broadcasting for Accelerometer and Magnetometer on Bangle.js
-// Upload this code to the watch via Espruino IDE
 
 var connected = false;
 
@@ -36,7 +35,30 @@ function onInit() {
   Bangle.setCompassPower(1);
   Bangle.setPollInterval(10); // 10ms for 100Hz
   Bangle.compassWr(0x31, 0x08); // Set mag to 100Hz ODR
+
+  // Configure accelerometer ODR to 100Hz via I2C
+  // Create a new I2C object and set up the pins.
+  var i2c = new I2C();
+  i2c.setup({scl:14, sda:15});
+
+  // Read CNTL1 register (0x18) to get the current value.
+  var cntl1 = i2c.readFrom(0x1E, 0x18, 1)[0];
+
+  // Clear the PC1 bit (Bit 7) to enter standby mode.
+  cntl1 &= ~(1 << 7); 
+  i2c.writeTo(0x1E, [0x18, cntl1]);
+
+  // Write the correct value (0x03) to ODCNTL register (0x1B) to set ODR to 100Hz.
+  i2c.writeTo(0x1E, [0x1B, 0x03]);
   
+  // Read back to verify
+  var odcntl = i2c.readFrom(0x1E, 0x1B, 1)[0];
+  print("ODCNTL read back:", odcntl.toString(16));
+  
+  // Set the PC1 bit (Bit 7) back to 1 to return to active mode.
+  cntl1 |= (1 << 7);
+  i2c.writeTo(0x1E, [0x18, cntl1]);
+
   NRF.on('connect', function() {
     connected = true;
   });
@@ -52,14 +74,7 @@ function onInit() {
         description: 'Magnetometer',
         notify: true,
         readable: true,
-        value: new Int32Array([0, 0, 0]).buffer,
-        writable: true,
-        onWrite: function(evt) {
-          var d = evt.data && evt.data[0];
-          if ([80, 40, 20, 10, 5].indexOf(d) >= 0) {
-            Bangle.setPollInterval(1000 / d);
-          }
-        }
+        value: new Int32Array([0, 0, 0]).buffer
       },
       'f8b23a4d-89ad-4220-8c9f-d81756009f0d': {
         description: 'Accelerometer',
@@ -76,8 +91,21 @@ function onInit() {
     services: ['f8b23a4d-89ad-4220-8c9f-d81756009f0c']
   });
 
-  Bangle.on('accel', onAccel);
-  Bangle.on('mag', onMag);
+  // Since 'accel' event doesn't work with changed ODR, use interval to poll
+  setInterval(() => {
+    var a = Bangle.getAccel();
+    onAccel(a);
+  }, 10);
+
+  // Since 'mag' event may not work reliably, use interval to poll
+  setInterval(() => {
+    var m = Bangle.getCompass();
+    onMag(m);
+  }, 10);
+
+  // Comment out the event listener
+  // Bangle.on('mag', onMag);
 }
 
 onInit();
+
