aa2d46dd4f6d2b6abeba3f4b171bba9cae23fc21
Saeid <saeidsobhani1371@gmail.com>
2025-10-08 14:29:53 +0200

    comment data rate increasing part

--- Simple_BLE_Broadcast.js
+++ Simple_BLE_Broadcast.js
@@ -33,16 +33,18 @@ function onAccel(d) {
 function onInit() {
   // Enable sensors always for testing
   Bangle.setCompassPower(1);
-  Bangle.setPollInterval(10); // 10ms for 100Hz
+  Bangle.setPollInterval(80); // 10ms for 100Hz
+  
+  /*
   Bangle.compassWr(0x31, 0x08); // Set mag to 100Hz ODR
 
-  // Configure accelerometer to 100Hz via I2C
+  Configure accelerometer to 100Hz via I2C
   var i2c = new I2C();
   i2c.setup({scl:14, sda:15});
   i2c.writeTo(0x1E, [0x18, 0b00101100]);  // Enter standby mode
   i2c.writeTo(0x1E, [0x1B, 0x03]);        // Set ODCNTL to 100Hz
   i2c.writeTo(0x1E, [0x18, 0b10101100]);  // Return to active mode
-
+  */
 
   NRF.on('connect', function() {
     connected = true;
@@ -76,7 +78,6 @@ function onInit() {
     services: ['f8b23a4d-89ad-4220-8c9f-d81756009f0c']
   });
 
-  // Comment out the event listener
   Bangle.on('mag', onMag);
   Bangle.on('accel', onAccel);
 }
